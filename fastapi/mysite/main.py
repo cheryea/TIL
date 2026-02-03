4a277bcdea96ec4d77876cef862e7a2a89e44972
cheryea <112612653+cheryea@users.noreply.github.com>
2026-02-03 15:29:59 +0900

    Create main.py

---
+++ fastapi/mysite/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from mysite.post_api import router as post_router
+
+app = FastAPI()
+app.include_router(post_router)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/hello")
+def hello():
+    return "Hello World!"
+
+
+@app.get("/hi")
+def hi():
+    return ["hello", "world", "!"]
+
+
+@app.get("/odd")
+def odd():
+    result = []
+    for i in range(100):
+        if i % 2 == 0:
+            result.append(i)
+
+    return result
